dbce88d39c4ad74b8b3c21594c967175dcce80d2
Quaid Rodgers <qmrodgers@eagles.usi.edu>
2022-02-23 23:36:24 -0600

    scrapped everything

--- Data/ITwilioRepo.cs
+++
@@ -1,7 +0,0 @@
-using TwilioAPI.Models;
-namespace TwilioAPI.Data {
-    public interface ITwilioRepo {
-        IEnumerable<TwilioApi> GetAll();
-        TwilioApi GetOutboundNumber(string callingNumber);
-    }
-}
--- Models/TwilioApi.cs
+++
@@ -1,8 +0,0 @@
-namespace TwilioAPI.Models {
-    public class TwilioApi {
-        public int Id { get; set; }
-        public string ClientNumber { get; set; }
-        public string EmployeeNumber { get; set; }
-        public string ClientName { get; set; }
-    }
-}
